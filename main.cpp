1775a30d7d089bf5334a5d02db62b24e5f5a730d
Yanwen Xu <xuyanwen2012@gmail.com>
2023-10-28 14:47:36 -0700

    worked!

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 
 #include <array>
 #include <iostream>
+#include <vector>
+#include <vulkan/vulkan_core.h>
 
 #include "VkBootstrap.h"
 #include "file_reader.hpp"
 
-#define VMA_IMPLEMENTATION
-#include "vk_mem_alloc.h"
+// #define VMA_IMPLEMENTATION
+// #include "vk_mem_alloc.h"
+#include "vma_usage.h"
 
 constexpr auto n = 1024;
+
+constexpr uint32_t computeShaderProcessUnit() { return 256; }
 
 struct Init {
   vkb::Instance instance;
@@ -32,7 +37,8 @@ struct ComputeData {
   VkPipeline compute_pipeline;
 
   VkCommandPool command_pool;
-  std::vector<VkCommandBuffer> command_buffers; // 1?
+  // std::vector<> command_buffers; // 1?
+  VkCommandBuffer command_buffer;
 };
 
 VkShaderModule createShaderModule(Init &init, const std::vector<char> &code) {
@@ -117,37 +123,6 @@ void vma_initialization(Init &init) {
   vmaCreateAllocator(&allocatorCreateInfo, &allocator);
 }
 
-// int create_descriptor_set_layout(Init &init, ComputeData &data) {
-//   std::array<VkDescriptorSetLayoutBinding, 3> layoutBindings{};
-//   layoutBindings[0].binding = 0;
-//   layoutBindings[0].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
-//   layoutBindings[0].descriptorCount = 1;
-//   layoutBindings[0].stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
-
-//   layoutBindings[1].binding = 1;
-//   layoutBindings[1].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
-//   layoutBindings[1].descriptorCount = 1;
-//   layoutBindings[1].stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
-
-//   layoutBindings[2].binding = 2;
-//   layoutBindings[2].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
-//   layoutBindings[2].descriptorCount = 1;
-//   layoutBindings[2].stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
-
-//   VkDescriptorSetLayoutCreateInfo layoutInfo{};
-//   layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
-//   layoutInfo.bindingCount = 3;
-//   layoutInfo.pBindings = layoutBindings.data();
-
-//   if (vkCreateDescriptorSetLayout(init.device, &layoutInfo, nullptr,
-//                                   &data.compute_descriptor_set_layout) !=
-//       VK_SUCCESS) {
-//     throw std::runtime_error("failed to create compute descriptor set
-//     layout!");
-//   }
-//   return 0;
-// }
-
 int create_descriptor_set_layout_v2(Init &init, ComputeData &data) {
   std::array<VkDescriptorSetLayoutBinding, 1> binding;
   binding[0].binding = 0;
@@ -170,110 +145,19 @@ int create_descriptor_set_layout_v2(Init &init, ComputeData &data) {
   return 0;
 }
 
-// int create_descriptor_pool(Init &init, ComputeData &data) {
-//   std::array<VkDescriptorPoolSize, 1> poolSizes{};
-//   poolSizes[0].type = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
-//   poolSizes[0].descriptorCount = 1;
+int create_command_pool(Init &init, ComputeData &data) {
+  VkCommandPoolCreateInfo pool_info = {};
+  pool_info.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+  pool_info.queueFamilyIndex =
+      init.device.get_queue_index(vkb::QueueType::compute).value();
 
-//   VkDescriptorPoolCreateInfo poolInfo = {};
-//   poolInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
-//   poolInfo.maxSets = 1;
-//   poolInfo.poolSizeCount = static_cast<uint32_t>(poolSizes.size());
-//   poolInfo.pPoolSizes = poolSizes.data();
-
-//   if (init.disp.createDescriptorPool(&poolInfo, nullptr,
-//                                      &data.descriptor_pool)) {
-//     std::cout << "failed to create descriptor pool\n";
-//     return -1;
-//   };
-
-//   // Allocate
-
-//   // Load & Create Shader Modules (1/3)
-//   const auto compute_shader_code = readFile("shaders/square.spv");
-//   VkShaderModule compute_module = createShaderModule(init,
-//   compute_shader_code);
-
-//   if (compute_module == VK_NULL_HANDLE) {
-//     std::cout << "failed to create shader module\n";
-//     return -1;
-//   }
-
-//   // Create a Pipeline Layout (2/3)
-//   VkPipelineLayoutCreateInfo pipeline_layout_info = {};
-//   pipeline_layout_info.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
-//   pipeline_layout_info.setLayoutCount = 1;
-//   pipeline_layout_info.pSetLayouts = &data.compute_descriptor_set_layout;
-
-//   if (init.disp.createPipelineLayout(&pipeline_layout_info, nullptr,
-//                                      &data.compute_pipeline_layout) !=
-//       VK_SUCCESS) {
-//     std::cout << "failed to create pipeline layout\n";
-//     return -1;
-//   }
-
-//   // Pipeline itself (3/3)
-//   VkPipelineShaderStageCreateInfo computeShaderStageInfo = {};
-//   computeShaderStageInfo.sType =
-//       VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
-//   computeShaderStageInfo.stage = VK_SHADER_STAGE_COMPUTE_BIT;
-//   computeShaderStageInfo.module = compute_module;
-//   computeShaderStageInfo.pName = "main";
-
-//   VkComputePipelineCreateInfo pipelineInfo = {};
-//   pipelineInfo.sType = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO;
-//   pipelineInfo.stage = computeShaderStageInfo;
-//   pipelineInfo.layout = data.compute_pipeline_layout;
-
-//   if (init.disp.createComputePipelines(VK_NULL_HANDLE, 1, &pipelineInfo,
-//                                        nullptr,
-//                                        &data.compute_pipeline) != VK_SUCCESS)
-//                                        {
-//     std::cout << "failed to create compute pipeline\n";
-//     return -1;
-//   }
-
-//   init.disp.destroyShaderModule(compute_module, nullptr);
-//   return 0;
-// }
-
-// int create_command_pool(Init &init, ComputeData &data) {
-//   VkCommandPoolCreateInfo pool_info = {};
-//   pool_info.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
-//   pool_info.queueFamilyIndex =
-//       init.device.get_queue_index(vkb::QueueType::compute).value();
-
-//   if (init.disp.createCommandPool(&pool_info, nullptr, &data.command_pool) !=
-//       VK_SUCCESS) {
-//     std::cout << "failed to create command pool\n";
-//     return -1;
-//   }
-//   return 0;
-// }
-
-// int create_command_buffers(Init &init, ComputeData &data) {
-//   data.command_buffers.resize(1);
-
-//   VkCommandBufferAllocateInfo allocInfo = {};
-//   allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
-//   allocInfo.commandPool = data.command_pool;
-//   allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-//   allocInfo.commandBufferCount = (uint32_t)data.command_buffers.size();
-
-//   if (init.disp.allocateCommandBuffers(
-//           &allocInfo, data.command_buffers.data()) != VK_SUCCESS) {
-//     std::cout << "failed to allocate command buffers\n";
-//     return -1;
-//   }
-
-//   for (size_t i = 0; i < data.command_buffers.size(); i++) {
-//     // TODO: record command buffer
-//   }
-
-//   return 0;
-// }
-
-// int create_sync_objects(Init &init, ComputeData &data) { return 0; }
+  if (init.disp.createCommandPool(&pool_info, nullptr, &data.command_pool) !=
+      VK_SUCCESS) {
+    std::cout << "failed to create command pool\n";
+    return -1;
+  }
+  return 0;
+}
 
 int create_compute_pipeline(Init &init, ComputeData &data) {
   // Load & Create Shader Modules (1/3)
@@ -399,31 +283,87 @@ inline void vkCheck(int result) {
   }
 }
 
-// int createStorageBuffer(Init &init, ComputeData &data) {
-//   VkBufferCreateInfo bufferInfo = {};
-//   bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-//   bufferInfo.size = n * sizeof(float);
-//   bufferInfo.usage = VK_BUFFER_USAGE_STORAGE_BUFFER_BIT;
-//   // bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
-//   // bufferInfo.queueFamilyIndexCount = 0;
-//   // bufferInfo.pQueueFamilyIndices = nullptr;
-//   // if (init.disp.createBuffer(&bufferInfo, nullptr, &data.storageBuffer) !=
-//   // VK_SUCCESS) {
-//   // std::cout << "failed to create buffer\n";
-//   // return -1;
-//   // }
+// allocate command buffer
+// record command buffer
+// submit
+// wait for queue idle
+// read data back
+int execute(Init &init, ComputeData &data, VkBuffer &buffer,
+            VmaAllocation &allocation, const std::vector<float> &input_data) {
+  std::cout << "input data:\n";
+  for (size_t i = 0; i < input_data.size(); ++i) {
+    if (i % 64 == 0 && i != 0)
+      std::cout << '\n';
+    std::cout << input_data[i];
+  }
+  std::cout << '\n';
 
-//   // VkBuffer buffer;
+  // -------
 
-//   VmaAllocationCreateInfo allocInfo = {};
-//   allocInfo.usage = VMA_MEMORY_USAGE_AUTO;
+  VkCommandBufferAllocateInfo alloc_info = {};
+  alloc_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+  alloc_info.commandPool = data.command_pool;
+  alloc_info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+  alloc_info.commandBufferCount = 1;
+  if (init.disp.allocateCommandBuffers(&alloc_info, &data.command_buffer) !=
+      VK_SUCCESS) {
+    std::cout << "failed to allocate command buffers\n";
+    return 1;
+  }
 
-//   VmaAllocation allocation;
-//   vmaCreateBuffer(allocator, &bufferInfo, &allocInfo, &data.storageBuffer,
-//                   &allocation, nullptr);
+  // ------- RECORD COMMAND BUFFER --------
+  VkCommandBufferBeginInfo begin_info = {};
+  begin_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+  init.disp.beginCommandBuffer(data.command_buffer, &begin_info);
 
-//   return 0;
-// }
+  init.disp.cmdBindPipeline(data.command_buffer, VK_PIPELINE_BIND_POINT_COMPUTE,
+                            data.compute_pipeline);
+  init.disp.cmdBindDescriptorSets(
+      data.command_buffer, VK_PIPELINE_BIND_POINT_COMPUTE,
+      data.compute_pipeline_layout, 0, 1, &data.descriptor_set, 0, nullptr);
+
+  const auto group_count_x =
+      static_cast<uint32_t>(input_data.size() / computeShaderProcessUnit());
+  init.disp.cmdDispatch(data.command_buffer, group_count_x, 1, 1);
+
+  init.disp.endCommandBuffer(data.command_buffer);
+
+  // -------
+
+  VkSubmitInfo submit_info = {};
+  submit_info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+  submit_info.commandBufferCount = 1;
+  submit_info.pCommandBuffers = &data.command_buffer;
+  submit_info.waitSemaphoreCount = 0;
+  submit_info.signalSemaphoreCount = 0;
+  if (vkQueueSubmit(data.compute_queue, 1, &submit_info, VK_NULL_HANDLE) !=
+      VK_SUCCESS) {
+    std::cout << "failed to submit queue\n";
+    return 1;
+  }
+
+  // wait the calculation to finish
+  if (vkQueueWaitIdle(data.compute_queue) != VK_SUCCESS)
+    throw std::runtime_error("failed to wait queue idle!");
+
+  std::vector<float> output_data(n);
+
+  void *mapped_data;
+  vmaMapMemory(allocator, allocation, &mapped_data);
+  memcpy(output_data.data(), mapped_data, n * sizeof(float));
+  vmaUnmapMemory(allocator, allocation);
+
+  // -------
+
+  std::cout << "output data:\n";
+  for (size_t i = 0; i < output_data.size(); ++i) {
+    if (i % 64 == 0 && i != 0)
+      std::cout << '\n';
+    std::cout << output_data[i];
+  }
+  std::cout << '\n';
+  return 0;
+}
 
 int main() {
   // ------------------ DATA ------------------
@@ -459,8 +399,8 @@ int main() {
 
   void *mappedMemory = nullptr;
   vmaMapMemory(allocator, allocation, &mappedMemory);
-
   memcpy(mappedMemory, h_data.data(), n * sizeof(float));
+  vmaUnmapMemory(allocator, allocation);
 
   vkCheck(create_descriptor_set_layout_v2(init, compute_data));
   vkCheck(create_compute_pipeline(init, compute_data));
@@ -468,11 +408,11 @@ int main() {
   vkCheck(create_descriptor_pool(init, compute_data));
   vkCheck(create_descriptor_set(init, compute_data, buffer));
 
-  vmaUnmapMemory(allocator, allocation);
+  vkCheck(create_command_pool(init, compute_data));
+
+  execute(init, compute_data, buffer, allocation, h_data);
 
   vmaDestroyBuffer(allocator, buffer, allocation);
-
-  // vkCheck(create_command_pool(init, compute_data));
 
   // vkCheck(create_command_buffers(init, compute_data));
   // vkCheck(create_sync_objects(init, compute_data));
